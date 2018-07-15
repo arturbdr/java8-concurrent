accd9f2e5c91b6bf28aa4f4566f9e7585b8ca7ab
Artur Drummond <arturbdr@gmail.com>
2018-07-14 23:47:08 -0300

    fix: remove sysout and add log

--- src/main/java/com/learn/RacingConditionFixedWithConcurrentMap.java
+++ src/main/java/com/learn/RacingConditionFixedWithConcurrentMap.java
@@ -1,5 +1,7 @@
 package com.learn;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -8,41 +10,34 @@ import java.util.stream.IntStream;
 
 import static com.learn.utils.ConcurrentUtils.stop;
 
-
+@Slf4j
 public class RacingConditionFixedWithConcurrentMap {
-    public static final String NUMBER = "number";
-    ExecutorService executorService = Executors.newFixedThreadPool(10);
-    ConcurrentHashMap<String, AtomicInteger> concurrentHashMap = new ConcurrentHashMap<>();
+
+    private static final String NUMBER = "number";
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(10);
+    private ConcurrentHashMap<String, AtomicInteger> concurrentHashMap = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
-
         RacingConditionFixedWithConcurrentMap instance = new RacingConditionFixedWithConcurrentMap();
         instance.executeIt();
-
-
     }
 
     private void executeIt() {
-        IntStream.range(0, 100).forEach(num -> {
-            executorService.submit(() -> {
-                incrementNumberInMap();
-            });
-        });
+        IntStream.range(0, 100).forEach(num -> executorService.submit(this::incrementNumberInMap));
 
-        Runnable read = () -> {
-            System.out.println("concurrentHashMap = " + concurrentHashMap.get(NUMBER));
-        };
+        Runnable read = () -> log.info("concurrentHashMap = {}", concurrentHashMap.get(NUMBER));
 
         executorService.submit(read);
         stop(executorService);
 
     }
 
-    public void incrementNumberInMap() {
+    private void incrementNumberInMap() {
         synchronized (this) {
             concurrentHashMap.putIfAbsent(NUMBER, new AtomicInteger(0));
             concurrentHashMap.replace(NUMBER, new AtomicInteger(concurrentHashMap.get(NUMBER).incrementAndGet()));
-            System.out.println("concurrentHashMap = " + concurrentHashMap.get(NUMBER));
+            log.info("concurrentHashMap = {}", concurrentHashMap.get(NUMBER));
         }
     }
 
